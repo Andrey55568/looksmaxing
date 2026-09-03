6a86f699b9872ba15aa06333b99e21da40d193d5
Andrey55568 <ruslan.domshinskiy@mail.ru>
2026-09-03 19:11:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from aiogram.client.default import DefaultBotProperties
 # ==================== КОНФИГ ====================
 BOT_TOKEN = "8706127340:AAHPeKEi1gQB9l1Tt9Ryxua93bRmF4K5lJs"
 ADMIN_ID = 8061549073
-WEBAPP_URL = "https://t.me/KenyChadPSL_bot"
+WEBAPP_URL = "WEBAPP_URL = "https://example.com""
 
 # ==================== БАЗА ДАННЫХ ====================
 DB_PATH = "looks.db"
